7943da2791a2f047e9443bad6045afa06a073524
chewbecca615 <54290152+chewbecca615@users.noreply.github.com>
2020-08-26 23:46:59 -0700

    completed

---
+++ Enemy.java
@@ -0,0 +1,56 @@
+public abstract class Enemy
+{
+    String name;
+    int might;
+    boolean isHostile;
+    static int countEnemies = 0;
+
+    public Enemy(String name, int might, boolean isHostile)
+    {
+        this.name = name;
+        this.might = might;
+        this.isHostile = isHostile;
+        countEnemies++;
+
+    }// end full constructor
+
+    public Enemy()
+    {
+        this.name = null;
+        this.might = 0;
+        this.isHostile = true;
+        countEnemies++;
+
+    }// end full constructor
+
+
+    //getters
+    public String getName()
+    {
+        return name;
+    }//end getname
+
+    public int getMight()
+    {
+        return might;
+    }// end getMight
+
+    //setters
+    public void setName(String name)
+    {
+        this.name = name;
+    } //end setname
+
+    public void setMight(int might)
+    {
+        this.might = might;
+    }
+
+    public String toString()
+    {
+        return "This Enemy has:" +
+                "\n\tName = " + name +
+                "\n\tmight = " + might +
+                "\n\tisHostile = " + isHostile;
+    } // end toString (there is only one!)
+}//end class Enemy
